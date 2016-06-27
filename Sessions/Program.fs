bcccd7f76cab9e4c1edc42c3c3bce1c39bcb5efd
Chris Smith <csmith@scottlogic.co.uk>
2016-06-27 11:01:52 +0100

    Get basic web service up and running

--- Sessions/Program.fs
+++ Sessions/Program.fs
@@ -1,7 +1,31 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿module Program
 
+open Microsoft.Owin.Hosting
+open System
+open System.Configuration
+open System.Threading
+
+
+(*
+    Do not run Visual Studio as Administrator!
+
+    Open a command prompt as Administrator and run the following command, replacing username with your username
+    netsh http add urlacl url=http://*:8080/ user=username
+*)
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+let main _ =
+    try
+        let baseUrl = ConfigurationManager.AppSettings.Get("BaseUrl")
+        if String.IsNullOrEmpty baseUrl then
+            failwith "Missing configuration value: 'BaseUrl'"
+
+        use server = WebApp.Start<Bristech.Srm.HttpConfig.Startup>(baseUrl)
+
+        let waitIndefinitelyWithToken = 
+            let cancelSource = new CancellationTokenSource()
+            cancelSource.Token.WaitHandle.WaitOne() |> ignore
+        0
+
+    with
+    | ex ->
+        1
