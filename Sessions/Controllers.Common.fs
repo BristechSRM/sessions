4fe59c399f2888885f50d85d2b59247565d98f40
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-06-29 15:03:05 +0100

    Add handle controller with post

---
+++ Sessions/Controllers.Common.fs
@@ -0,0 +1,16 @@
+﻿module Controllers.Common
+
+open System
+open System.Net
+open System.Net.Http
+open System.Web.Http
+
+let Try (c: ApiController) successCode op =
+    try
+        c.Request.CreateResponse(successCode, op())
+    with
+    | ex ->
+        c.Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message)
+
+
+
---
+++ Sessions/HandlesController.fs
@@ -0,0 +1,15 @@
+﻿namespace Controllers
+
+open System
+open System.Net
+open System.Net.Http
+open System.Web.Http
+
+open Controllers.Common
+open HandlesRepository
+
+type HandlesController() = 
+    inherit ApiController()
+
+    member x.Post(handle: Models.Handle) = Try x HttpStatusCode.Created (fun () -> handle |> DataTransform.Handles.toEntity |> add)
+   
