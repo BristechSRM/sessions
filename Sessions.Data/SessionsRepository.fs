f36ea8917f61c5734c382573ed7646d034f846fe
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-06-30 16:15:56 +0100

    Add get all in sessionsRepository

--- Sessions.Data/SessionsRepository.fs
+++ Sessions.Data/SessionsRepository.fs
@@ -1,6 +1,8 @@
 ﻿module SessionsRepository
 
 open System
+open Dapper
+open Dapper.Contrib.Extensions
 open Database
 open Entities
 
@@ -8,3 +10,5 @@ let add (session: Session) =
     let guid = Guid.NewGuid()
     insert {session with Id = guid; Date = Nullable DateTime.UtcNow;} |> ignore
     guid
+
+let getAll () = Database.getConnection().GetAll<Session>()
