6de662da08c4772065ebbb1cd1d78224753448cb
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-06-30 10:45:54 +0100

    Add handle get repository function

--- Sessions.Data/HandlesRepository.fs
+++ Sessions.Data/HandlesRepository.fs
@@ -1,4 +1,12 @@
 ﻿module HandlesRepository
 
-let add (handle: Entities.Handle) = Database.insert handle |> ignore
-    
+open Dapper
+open Database
+open System
+
+let add (handle : Entities.Handle) = insert handle |> ignore
+
+let get (handletype : string) (identifier : string) = 
+    getConnection().Query<Entities.Handle> ("select profileId, type, identifier from handles where type = @Type and identifier = @Identifier", 
+                                            dict [ "Type", box handletype
+                                                   "Identifier", box identifier ])
