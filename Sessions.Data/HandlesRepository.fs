d9f48a298494c9f8d8b9834fea754b8f14291eaf
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-06-30 11:11:55 +0100

    Add handle get by profileId

--- Sessions.Data/HandlesRepository.fs
+++ Sessions.Data/HandlesRepository.fs
@@ -7,7 +7,9 @@ open Entities
 
 let add (handle : Handle) = insert handle |> ignore
 
-let get (htype : string) (identifier : string) = 
-    getConnection().Query<Handle> ("select profileId, type, identifier from handles where type = @Type and identifier = @Identifier", 
-                                            dict [ "Type", box htype
+let getByTypeAndIdentifier (handletype : string) (identifier : string) =
+    getConnection().Query<Entities.Handle> ("select profileId, type, identifier from handles where type = @Type and identifier = @Identifier",
+                                            dict [ "Type", box handletype
                                                    "Identifier", box identifier ])
+
+let getByProfileId (profileId : Guid) = getConnection().Query<Entities.Handle> ("select profileId, type, identifier from handles where profileId = @ProfileId", dict [ "ProfileId", box profileId ])
