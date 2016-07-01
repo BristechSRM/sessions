b40638b819aaf04b604852ced8ec653fcabcb2cf
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-07-01 10:30:43 +0100

    new lines

--- Sessions/HandlesController.fs
+++ Sessions/HandlesController.fs
@@ -13,6 +13,7 @@ type HandlesController() =
 
     member x.Post(handle : Models.Handle) = Catch.respond x HttpStatusCode.Created (fun () -> handle |> Handles.toEntity |> add)
 
-    member x.Get(htype : string, identifier: string) = Catch.respond x HttpStatusCode.OK (fun () -> getByTypeAndIdentifier htype identifier |> Seq.head |> Handles.toModel)
+    member x.Get(htype : string, identifier: string) = 
+        Catch.respond x HttpStatusCode.OK (fun () -> getByTypeAndIdentifier htype identifier |> Seq.head |> Handles.toModel)
 
     member x.Get(profileId : Guid) = Catch.respond x HttpStatusCode.OK (fun () -> getByProfileId profileId |> Seq.head |> Handles.toModel)
