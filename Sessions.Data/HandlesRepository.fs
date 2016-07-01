b58f770bd8032a655be8f4a7c4cef1436885c993
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-07-01 09:45:07 +0100

    Cleanup

--- Sessions.Data/HandlesRepository.fs
+++ Sessions.Data/HandlesRepository.fs
@@ -7,9 +7,9 @@ open Entities
 
 let add (handle : Handle) = insert handle |> ignore
 
-let getByTypeAndIdentifier (handletype : string) (identifier : string) =
-    getConnection().Query<Entities.Handle> ("select profileId, type, identifier from handles where type = @Type and identifier = @Identifier",
-                                            dict [ "Type", box handletype
+let getByTypeAndIdentifier (htype : string) (identifier : string) =
+    getConnection().Query<Handle> ("select profileId, type, identifier from handles where type = @Type and identifier = @Identifier",
+                                            dict [ "Type", box htype
                                                    "Identifier", box identifier ])
 
-let getByProfileId (profileId : Guid) = getConnection().Query<Entities.Handle> ("select profileId, type, identifier from handles where profileId = @ProfileId", dict [ "ProfileId", box profileId ])
+let getByProfileId (profileId : Guid) = getConnection().Query<Handle> ("select profileId, type, identifier from handles where profileId = @ProfileId", dict [ "ProfileId", box profileId ])
