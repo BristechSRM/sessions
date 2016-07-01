ffc6ee2da0af70fb4ef32c67ecaf2dde315ffb5e
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-07-01 15:13:34 +0100

    Correct function so that sequence of handles is returned

--- Sessions/HandlesController.fs
+++ Sessions/HandlesController.fs
@@ -16,6 +16,6 @@ type HandlesController() =
     member x.Get(htype : string, identifier: string) =
         Catch.respond x HttpStatusCode.OK (fun () -> getByTypeAndIdentifier htype identifier |> Seq.head |> Handles.toModel)
 
-    member x.Get(profileId : Guid) = Catch.respond x HttpStatusCode.OK (fun () -> getByProfileId profileId |> Seq.head |> Handles.toModel)
+    member x.Get(profileId : Guid) = Catch.respond x HttpStatusCode.OK (fun () -> getByProfileId profileId |> Seq.map Handles.toModel)
 
     member x.Get() = Catch.respond x HttpStatusCode.OK (fun () -> getAll() |> Seq.map Handles.toModel)
