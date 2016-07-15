738effec2c7977290e9772a973d002d0e2ebcfa4
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-07-15 11:03:14 +0100

    Add handle patch for identifier

--- Sessions/HandlesController.fs
+++ Sessions/HandlesController.fs
@@ -4,12 +4,17 @@ open System
 open System.Net
 open System.Net.Http
 open System.Web.Http
-
 open HandlesRepository
+open RestModels
 open DataTransform
 
 type HandlesController() =
     inherit ApiController()
+
+    let patch (handleId: int) (op: PatchOp) = 
+        match op.Path with
+        | "identifier" -> updateField handleId op.Path op.Value
+        | _ ->  raise <| Exception(sprintf "Error: Patch currently does not accept: %s for handles" op.Path)   
 
     member x.Post(handle : Models.Handle) = (fun () -> handle |> Handle.toEntity |> add) |> Catch.respond x HttpStatusCode.Created 
 
@@ -21,3 +26,5 @@ type HandlesController() =
     member x.Get() = (fun () -> getAll() |> Seq.map Handle.toModel) |> Catch.respond x HttpStatusCode.OK 
 
     member x.Get(id: int) = (fun () -> get id |> Handle.toModel) |> Catch.respond x HttpStatusCode.OK
+
+    member x.Patch(id: int, op: PatchOp) = (fun () -> patch id op) |> Catch.respond x HttpStatusCode.NoContent
