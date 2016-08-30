b90591b08d64356ecfc103e313cc1be7fef2c3c8
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-08-30 10:31:39 +0100

    Remove unused imports

--- Sessions/SessionIdController.fs
+++ Sessions/SessionIdController.fs
@@ -2,13 +2,8 @@
 
 open System
 open System.Net
-open System.Net.Http
 open System.Web.Http
 open SessionsRepository
-open DataTransform
-open Models
-open RestModels
-
 
 type SessionIdController() = 
     inherit ApiController()
