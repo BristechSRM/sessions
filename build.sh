e857ddf0d32a7294c5c107da56f1ef3acd526102
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-08-31 11:15:34 +0100

    Update scripts for windows running with git bash

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-mono packages/FAKE/tools/FAKE.exe build.fsx
+if test "$OS" = "Windows_NT"
+then
+    packages/FAKE/tools/FAKE.exe build.fsx
+else
+    mono packages/FAKE/tools/FAKE.exe build.fsx
+fi
--- setup.sh
+++ setup.sh
@@ -8,7 +8,13 @@ fi
 if [ ! -f ".nuget/nuget.exe" ]; then
     echo "Fetching latest nuget executable"
     cd .nuget
-    wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+    if test "$OS" = "Windows_NT"
+    then
+        echo "Note: On windows you need to have curl installed and in your path for this fetch to work, or just download the nuget.exe from"
+        echo "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+        echo "And place under the .nuget folder"        
+    fi
+    curl -O https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
     cd ..
 else
     echo "Nuget executable found"
@@ -16,7 +22,13 @@ fi
 
 if [ ! -f "packages/FAKE/tools/FAKE.exe" ]; then
     echo "Installing FAKE package"
-    mono .nuget/nuget.exe install "FAKE" -OutputDirectory packages/ -ExcludeVersion
+
+    if test "$OS" = "Windows_NT"
+    then
+        .nuget/nuget.exe install "FAKE" -OutputDirectory packages/ -ExcludeVersion
+    else
+        mono .nuget/nuget.exe install "FAKE" -OutputDirectory packages/ -ExcludeVersion
+    fi
 else
     echo "Fake Package found"
 fi
