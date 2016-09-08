a5592bab31d57c8d61d71e01c4ceae926d1f259a
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-09-08 11:34:32 +0100

    Add event patch for published Date

--- Sessions.Data/EventsRepository.fs
+++ Sessions.Data/EventsRepository.fs
@@ -13,3 +13,5 @@ let add (event: Event) =
   let guid = Guid.NewGuid()
   insert { event with Id = guid } |> ignore
   guid
+
+let updateField (eventId : Guid) (propName : string) newValue = Database.updateField<Event> eventId propName newValue
