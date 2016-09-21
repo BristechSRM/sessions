f211b96114f5cb9c82e459d8ae22670ad7968e1e
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-09-21 16:07:25 +0100

    Add meetup event delete and remove from event record

--- Sessions.Data/MeetupEventsRepository.fs
+++ Sessions.Data/MeetupEventsRepository.fs
@@ -7,9 +7,11 @@ open System
 
 let getAll () = getConnection().GetAll<MeetupEvent>()
 
-let get (id : Guid) = getConnection().Get<MeetupEvent>(id)
+let get (meetupEventId : Guid) = getConnection().Get<MeetupEvent>(meetupEventId)
 
 let add (me: MeetupEvent) =
     let guid = Guid.NewGuid()
     insert { me with Id = guid } |> ignore
     guid
+
+let delete (meetupEventId : Guid) = deleteById<MeetupEvent> meetupEventId
