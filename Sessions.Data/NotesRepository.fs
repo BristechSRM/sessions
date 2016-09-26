51bb3e06ac2f3f7935601136fb0470712e49aabf
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-09-26 16:43:17 +0100

    Add get notes by sessionId

--- Sessions.Data/NotesRepository.fs
+++ Sessions.Data/NotesRepository.fs
@@ -8,3 +8,5 @@ open Entities
 
 //TODO Test method only. Remove when ready
 let getAll() = getConnection().GetAll<Note>()
+
+let getNotesBySessionId (sessionId : Guid) = selectWhere<Note> (dict [ "SessionId", box sessionId ])
--- Sessions/NotesController.fs
+++ Sessions/NotesController.fs
@@ -5,9 +5,13 @@ open NotesRepository
 open System.Net
 open System.Net.Http
 open System.Web.Http
+open System
 
 
 type NotesController() = 
     inherit ApiController()
     
     member x.Get() = (fun () -> getAll() |> Seq.map Note.toModel) |> Catch.respond x HttpStatusCode.OK
+
+    [<HttpGet>]
+    member x.GetBySessionId(sessionId : Guid) = (fun () -> getNotesBySessionId sessionId) |> Catch.respond x HttpStatusCode.OK
