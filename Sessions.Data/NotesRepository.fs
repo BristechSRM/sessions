7b4e6d99301b5a1675a01a4445c56d228c2ddb6a
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-09-26 16:30:39 +0100

    Add notes controller, repository and piping

---
+++ Sessions.Data/NotesRepository.fs
@@ -0,0 +1,10 @@
+﻿module NotesRepository
+
+open System
+open Dapper
+open Dapper.Contrib.Extensions
+open Database
+open Entities
+
+//TODO Test method only. Remove when ready
+let getAll() = getConnection().GetAll<Note>()
---
+++ Sessions/NotesController.fs
@@ -0,0 +1,13 @@
+﻿namespace Controllers
+
+open DataTransform
+open NotesRepository
+open System.Net
+open System.Net.Http
+open System.Web.Http
+
+
+type NotesController() = 
+    inherit ApiController()
+    
+    member x.Get() = (fun () -> getAll() |> Seq.map Note.toModel) |> Catch.respond x HttpStatusCode.OK
