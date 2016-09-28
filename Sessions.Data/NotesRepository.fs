063f90ea7ef33729947cb2b334e022e1b6e5ee20
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-09-28 11:49:41 +0100

    Add Post Note endpoint

--- Sessions.Data/NotesRepository.fs
+++ Sessions.Data/NotesRepository.fs
@@ -6,6 +6,11 @@ open Dapper.Contrib.Extensions
 open Database
 open Entities
 
+let add (note : Note) = 
+    let guid = Guid.NewGuid()
+    insert { note with Id = guid; DateAdded = DateTime.UtcNow; DateModified = DateTime.UtcNow } |> ignore
+    guid
+
 //TODO Test method only. Remove when ready
 let getAll() = getConnection().GetAll<Note>()
 
--- Sessions/NotesController.fs
+++ Sessions/NotesController.fs
@@ -5,6 +5,7 @@ open NotesRepository
 open System.Net
 open System.Net.Http
 open System.Web.Http
+open Models
 open System
 
 
@@ -14,4 +15,6 @@ type NotesController() =
     member x.Get() = (fun () -> getAll() |> Seq.map Note.toModel) |> Catch.respond x HttpStatusCode.OK
 
     [<HttpGet>]
-    member x.GetBySessionId(sessionId : Guid) = (fun () -> getNotesBySessionId sessionId) |> Catch.respond x HttpStatusCode.OK
+    member x.GetBySessionId(sessionId : Guid) = (fun () -> getNotesBySessionId sessionId |> Seq.map Note.toModel) |> Catch.respond x HttpStatusCode.OK
+
+    member x.Post(note : Note) = (fun () -> note |> Note.toEntity |> add) |> Catch.respond x HttpStatusCode.Created
