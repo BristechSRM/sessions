f819c3734d849181ed9bf89e4391626063603a9f
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-10-06 14:34:56 +0100

    Profile handles patch

--- Sessions.Data/HandlesRepository.fs
+++ Sessions.Data/HandlesRepository.fs
@@ -18,4 +18,38 @@ let getAll () = getConnection().GetAll<Handle>()
 
 let get (handleId : int) = getConnection().Get<Handle>(handleId)
 
+let delete (handleId : int) = deleteById<Handle> handleId
+
 let updateField (handleId: int) (propName: string) newValue = Database.updateField<Handle> handleId propName newValue 
+
+let putHandles (profileId : Guid) (editedHandles : Handle []) = 
+    let oldHandles = getByProfileId profileId |> Seq.toArray
+
+    let newHandles = 
+        editedHandles 
+        |> Array.filter (fun h -> oldHandles |> Array.exists(fun oldH -> h.Id = oldH.Id ) |> not)
+
+    let updatedHandles = 
+        editedHandles 
+        |> Array.filter (fun h -> 
+            let found = oldHandles |> Array.tryFind(fun oldH -> h.Id = oldH.Id )
+            match found with
+            | None -> false
+            | Some oldH -> h.Type <> oldH.Type || h.Identifier <> oldH.Identifier)
+
+    let deletedHandles = 
+        oldHandles
+        |> Array.filter (fun oldH -> editedHandles |> Array.exists(fun newH -> oldH.Id = newH.Id) |> not)
+
+    newHandles
+    |> Array.iter (fun handle -> 
+        add {handle with ProfileId = profileId} |> ignore)
+
+    updatedHandles 
+    |> Array.iter (fun handle -> update<Handle> {handle with ProfileId = profileId} |> ignore)
+
+
+    deletedHandles 
+    |> Array.iter (fun handle -> delete handle.Id |> ignore)
+    0
+    
